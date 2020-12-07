6f75bf3d4cdbfe733642af077c1d55b6aa2997aa
vladho <vladhopilo@gmail.com>
2020-12-07 02:14:11 +0200

    task1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const massiv = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+const logItems = function(names) {
+
+for (let i = 0; i < massiv.length; i += 1){
+    
+    console.log(i + 1 + " - " + massiv[i])
+
+}
+
+
+}
+
+logItems(massiv)
