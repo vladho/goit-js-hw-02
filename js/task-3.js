4bd27f57983c279c317bd0f8aec010490e9799fb
Vlad <vladhopilo@gmail.com>
2020-12-08 15:48:19 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,15 @@
 const findLongestWord = function (string) {
     const words = string.split(" ");
-    // console.log(words);
-    // console.log(words.length);
-    let longestWord = 0;
-    // let longestWord = 0;
-    for (let i = 0; i < words.length; i += 1) {
-        // console.log(words[4]);
-        // console.log(Object.values(words[i]));
-        console.log(words[4].length);
-        // console.log(longestWord);
-        if (words[i].length > longestWord) {
-            longestWord = words[i].length;
-            // console.log(words[longestWord]);
-            // console.log(longestWord);
+      let long = 0;
+    let longestWord;
+       for (let i = 0; i < words.length; i += 1) {
+        if (words[i].length > long) {
+            long = words[i].length;
+            longestWord = Object.values(words[i]);
         }
-// console.log(longestWord);
-        // else
-        // longestWord = "false";
     }
-    // return longestWord = words[longestWord];
-    console.log(longestWord);
-    // console.log(words[i]);
-
+    return longestWord.join("")
 }
 
-const r1 = findLongestWord("The quick brown fox jumped over the lazy dog");
+const r1 = findLongestWord("May the force be with you");
 console.log(r1);
-// findLongestWord("The quick brown fox jumped over the lazy dog");
